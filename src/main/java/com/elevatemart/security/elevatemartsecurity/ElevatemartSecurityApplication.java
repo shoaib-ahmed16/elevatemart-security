cd2f1388c8fc945bdb8965e72d58223dd9c0e796
shoaib-ahmed16 <shoaib-ahmed16>
2023-12-16 16:26:41 +0530

    Adding project

---
+++ src/main/java/com/elevatemart/security/elevatemartsecurity/ElevatemartSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.elevatemart.security.elevatemartsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElevatemartSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElevatemartSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elevatemart/security/elevatemartsecurity/ElevatemartSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elevatemart.security.elevatemartsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElevatemartSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
